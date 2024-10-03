98663d2a74652623b278f44dada2a2cc504b1712
Nalinaka <nalinaka@gmail.com>
2024-10-03 11:18:26 -0700

    Working on visibility of modal

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-
-
-function contact (event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
-    emailjs
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
     .sendForm(
-        'service_nmjm2wt',
-        'template_nypeksg',
-        event.target,
-        'o2MKta5yqq3mx3fc'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }).catch (() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert (
-        "The email service is temporarily unavailable, fix this I will. Please contact me directly on nalinionair@gmail.com"
-        );
+      "service_nmjm2wt",
+      "template_nypeksg",
+      event.target,
+      "o2MKta5yqq3mx3fc"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
     })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable, fix this I will. Please contact me directly on nalinionair@gmail.com"
+      );
+    });
 }
 
 function ToggleModal() {
-    // toggle modal
+  // toggle modal
 }
